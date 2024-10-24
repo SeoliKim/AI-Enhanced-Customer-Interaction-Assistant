5dc867ca39244511d90f3e454e4429ab3e18ea95
Irine-Juliet <irenejuliet00@gmail.com>
2024-10-24 14:25:46 -0400

    implemented assistant

---
+++ backend/assistant/assistant.py
@@ -0,0 +1,24 @@
+from langchain_core.runnables import Runnable, RunnableConfig
+from models.state import State
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            configuration = config.get("configurable", {})
+            user_id = configuration.get("user_id", None)
+            state = {**state, "user_info": user_id}
+            result = self.runnable.invoke(state)
+            
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Please provide a complete response.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
