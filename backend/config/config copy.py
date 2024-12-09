991f29b7c47aa8374dcca7dc4842b2d25fa05a5e
SeoliKim <seolikim1225@gmail.com>
2024-12-08 23:42:06 -0500

    final integration

---
+++ backend/config/config copy.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    DB_PATH = '../backend/database/ecommerce_data.db'
+    MODEL_NAME = "gpt-3.5-turbo"
+    TEMPERATURE = 0.7
+    DEFAULT_USER = "example_user_123"
+    API_KEY = ""
